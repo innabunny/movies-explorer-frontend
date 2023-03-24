7e1d702acecf8bf3146a0eedfd8d6c37040edeae
Inna Gosteva <innagosteva31@yandex.ru>
2023-03-25 01:10:23 +0500

    fix/edit user

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -125,6 +125,8 @@ function App() {
       .then((updateUser) => {
         setLoggedIn(true);
         setCurrentUser(updateUser);
+        localStorage.setItem("userName", updateUser.name);
+        localStorage.setItem("userEmail", updateUser.email);
         setErrorMessageProfile('Профиль успешно обновлен!');
       })
       .catch((err) => {
@@ -239,7 +241,6 @@ function App() {
                     errMessageProfile={errorMessageProfile}
                     handleUpdateDataUser={handleUpdateUser}
                     handleLogout={handleLogout}
-                    currentUser={currentUser}
                   />
                 </>
               </ProtectedRoute>
